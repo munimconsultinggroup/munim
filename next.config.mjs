2cf90f9aa8578f9a2d38f0d3632e26a12f38dcbf
urs-santoshh <santosh18timalsina@gmail.com>
2024-08-01 17:07:42 +0545

    for deploying in github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+   output: "export",
+
+   /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+   basePath: "/munim",
+};
 
 export default nextConfig;
